79ba332321eb34aa629d4f665471158c51620fd7
Кирилл Зикевский <2309696@mail.ru>
2024-03-11 12:29:41 +0300

    dev: Decimal project in progress

---
+++ C/Decimal/src/other/other.h
@@ -0,0 +1,11 @@
+#ifndef S21_HELPERS_OTHER_H
+#define S21_HELPERS_OTHER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../arithmetic_operations/helpers.h"
+#include "../helpers/helpers.h"
+#include "../s21_decimal.h"
+
+#endif
