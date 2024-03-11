255d1b61ef5033b73c57d4debe9d86409d60749b
Кирилл Зикевский <2309696@mail.ru>
2024-03-11 12:30:42 +0300

    dev: Decimal project in progress

---
+++ C/Decimal/src/other/s21_floor.c
@@ -0,0 +1,20 @@
+#include "other.h"
+
+int s21_floor(s21_decimal value, s21_decimal *result) {
+  int status_code = 0;
+  if (!result)
+    status_code = 1;
+  else {
+    s21_decimal fraction = init_decimal();
+    s21_truncate(value, result);
+    int sign = s21_get_sign(value);
+    s21_sub(value, *result, &fraction);
+    if (!is_zero_decimal(fraction) && sign) {
+      s21_decimal one = init_decimal();
+      one.bits[0] = 1;
+      s21_sub(*result, one, result);
+    }
+    s21_set_sign(result, sign);
+  }
+  return status_code;
+}
