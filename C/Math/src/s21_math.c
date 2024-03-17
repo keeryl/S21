9b4804d358473c1f5854c8026c65ffa1e92616e4
Кирилл Зикевский <2309696@mail.ru>
2024-03-17 11:41:59 +0300

    dev: Math project in progress

--- C/Math/src/s21_math.c
+++ C/Math/src/s21_math.c
@@ -9,3 +9,19 @@ int s21_abs(int x) {
   }
   return result;
 }
+
+long double s21_acos(double x) {
+  long double res = 0.0;
+  if (x == 1)
+    res = +0.0;
+  else if (s21_fabs(x) > 1 || x != x)
+    res = S21_NAN;
+  else if (x == 0.0)
+    res = 1.5707963267948965579989817342720925807952880859375;
+  else if (x > 0)
+    res = s21_atan(s21_sqrt(1.0 - (x * x)) / x);
+  else
+    res = S21_PI + s21_atan(s21_sqrt(1.0 - (x * x)) / x);
+  return res;
+}
+
