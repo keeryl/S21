301e2d70a63a24996461163a0e125d72b8f4fdc3
Кирилл Зикевский <2309696@mail.ru>
2024-03-18 18:02:33 +0300

    dev: Math project in progress

--- C/Math/src/s21_math.c
+++ C/Math/src/s21_math.c
@@ -39,3 +39,30 @@ long double s21_asin(double x) {
     res = s21_atan(x / s21_sqrt(1.0 - (x * x)));
   return res;
 }
+
+long double s21_atan(double x) {
+  long double result = 0.0;
+  if (x != x)
+    result = S21_NAN;
+  else if (x == 0.0)
+    result = 0.0;
+  else if (x == 1.0)
+    result = 0.78539816339744827899949086713604629039764404296875;
+  else if (x == -1.0)
+    result = -0.78539816339744827899949086713604629039764404296875;
+  else if (s21_fabs(x) == S21_INF)
+    result = (x < 0) ? -S21_PI / 2.0 : S21_PI / 2.0;
+  else if (x > 1)
+    result = S21_PI / 2.0 - s21_atan(1.0 / x);
+  else if (x < -1)
+    result = -S21_PI / 2.0 - s21_atan(1.0 / x);
+  else if (-1 < x && x < 1) {
+    result = x;
+    long double add = x;
+    for (long long i = 1; i < 2000; i++) {
+      add *= -x * x;
+      result += add / (2.0 * i + 1.0);
+    }
+  }
+  return result;
+}
