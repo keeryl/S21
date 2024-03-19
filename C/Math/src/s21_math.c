625349f0ec905b53dfc4e29e0f6fc850891460cf
Кирилл Зикевский <2309696@mail.ru>
2024-03-19 13:31:44 +0300

    dev: Math project in progress

--- C/Math/src/s21_math.c
+++ C/Math/src/s21_math.c
@@ -156,3 +156,21 @@ long double s21_floor(double x) {
   if (x < 0. && s21_fabs(x - result) > 0.) result -= 1;
   return result;
 }
+
+long double s21_fmod(double x, double y) {
+  long double res = 0.0;
+  long double div_res = x / y;
+  if (x == S21_INF || x == -S21_INF || y == 0 || y != y || x != x)
+    res = S21_NAN;
+  else if (x == 0 && y == y)
+    res = 0.0;
+  else if ((y == S21_INF || y == -S21_INF) && (x != S21_INF && x != -S21_INF))
+    res = x;
+  else if ((x > 0 && x < y) || (x < 0 && x > y))
+    res = x;
+  else if (div_res > 0)
+    res = x - y * s21_floor(div_res);
+  else if (div_res < 0)
+    res = x - y * s21_ceil(div_res);
+  return res;
+}
