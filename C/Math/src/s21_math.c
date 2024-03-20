455f2876f3e443d640f88d01c87b5e2217137881
Кирилл Зикевский <2309696@mail.ru>
2024-03-20 13:56:41 +0300

    dev: Math project in progress

--- C/Math/src/s21_math.c
+++ C/Math/src/s21_math.c
@@ -265,3 +265,14 @@ long double s21_sin(double x) {
   }
   return res;
 }
+
+long double s21_sqrt(double x) {
+  long double res = 0.0;
+  if (x == -0)
+    res = -0.0;
+  else if (x < 0)
+    res = S21_NAN;
+  else
+    res = s21_pow(x, 0.5);
+  return res;
+}
