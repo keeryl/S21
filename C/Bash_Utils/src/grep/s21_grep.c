8f19b5c447934df78c8c5159c40ac55d8b6582e9
Кирилл Зикевский <2309696@mail.ru>
2024-03-26 16:04:01 +0300

    dev: Bash_Utils project in progress

--- C/Bash_Utils/src/grep/s21_grep.c
+++ C/Bash_Utils/src/grep/s21_grep.c
@@ -20,3 +20,52 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+void parse_args(int argc, char **argv, grep_flags *flags, char **patterns,
+                char **patterns_files, int *patterns_count,
+                int *patterns_files_count) {
+  static struct option long_options[] = {
+      {0, 0, 0, 0},
+  };
+  int option, option_index;
+  while ((option = getopt_long(argc, argv, "e:ivclnhsf:o", long_options,
+                               &option_index)) != -1) {
+    switch (option) {
+      case 'e':
+        flags->e = 1;
+        process_flag_e(patterns, patterns_count);
+        break;
+      case 'i':
+        flags->i = 1;
+        break;
+      case 'v':
+        flags->v = 1;
+        break;
+      case 'c':
+        flags->c = 1;
+        break;
+      case 'l':
+        flags->l = 1;
+        break;
+      case 'n':
+        flags->n = 1;
+        break;
+      case 'h':
+        flags->h = 1;
+        break;
+      case 's':
+        flags->s = 1;
+        break;
+      case 'f':
+        flags->f = 1;
+        process_flag_f(patterns_files, patterns_files_count);
+        break;
+      case 'o':
+        flags->o = 1;
+        break;
+      default:
+        fprintf(stderr, "usage: s21_grep [-eivclnhsfo] [file ...]\n");
+        exit(1);
+        break;
+    }
+  }
+}
