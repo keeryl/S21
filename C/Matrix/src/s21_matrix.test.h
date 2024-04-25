7b0c714d6978ba73a962b5fb8855123babdeacb6
Кирилл Зикевский <2309696@mail.ru>
2024-04-25 12:57:41 +0300

    dev: Matrix project in progress

--- C/Matrix/src/s21_matrix.test.h
+++ C/Matrix/src/s21_matrix.test.h
@@ -624,4 +624,23 @@ START_TEST(calc_complements_1) {
 }
 END_TEST
 
+START_TEST(calc_complements_2) {
+  matrix_t A = {0};
+  s21_create_matrix(3, 3, &A);
+  A.matrix[0][0] = NAN;
+  A.matrix[0][1] = 2;
+  A.matrix[0][2] = 3;
+  A.matrix[1][0] = 0;
+  A.matrix[1][1] = 4;
+  A.matrix[1][2] = 2;
+  A.matrix[2][0] = 5;
+  A.matrix[2][1] = 2;
+  A.matrix[2][2] = 1;
+  matrix_t res = {0};
+  int status_code = s21_calc_complements(&A, &res);
+  ck_assert_int_eq(status_code, CALC_ERR);
+  s21_remove_matrix(&A);
+}
+END_TEST
+
 #endif
