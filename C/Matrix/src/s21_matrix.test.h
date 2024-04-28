0ef032613916949649d3d00654186de9078914b4
Кирилл Зикевский <2309696@mail.ru>
2024-04-28 21:28:11 +0300

    dev: project Matrix in progress

--- C/Matrix/src/s21_matrix.test.h
+++ C/Matrix/src/s21_matrix.test.h
@@ -842,4 +842,23 @@ START_TEST(inverse_2) {
 }
 END_TEST
 
+START_TEST(inverse_3) {
+  matrix_t A = {0};
+  s21_create_matrix(3, 3, &A);
+  A.matrix[0][0] = NAN;
+  A.matrix[0][1] = 5;
+  A.matrix[0][2] = 7;
+  A.matrix[1][0] = 6;
+  A.matrix[1][1] = 3;
+  A.matrix[1][2] = 4;
+  A.matrix[2][0] = 5;
+  A.matrix[2][1] = -2;
+  A.matrix[2][2] = -3;
+  matrix_t res = {0};
+  int status_code = s21_inverse_matrix(&A, &res);
+  ck_assert_int_eq(status_code, CALC_ERR);
+  s21_remove_matrix(&A);
+}
+END_TEST
+
 #endif
