a2c005d5a11d5abaf22a827f39dc3a1ba2f88305
Кирилл Зикевский <2309696@mail.ru>
2024-04-28 21:27:29 +0300

    dev: project Matrix in progress

--- C/Matrix/src/s21_matrix.test.h
+++ C/Matrix/src/s21_matrix.test.h
@@ -785,4 +785,42 @@ START_TEST(determinant_6) {
 }
 END_TEST
 
+START_TEST(inverse_1) {
+  matrix_t A = {0};
+  s21_create_matrix(3, 3, &A);
+  A.matrix[0][0] = 2;
+  A.matrix[0][1] = 5;
+  A.matrix[0][2] = 7;
+  A.matrix[1][0] = 6;
+  A.matrix[1][1] = 3;
+  A.matrix[1][2] = 4;
+  A.matrix[2][0] = 5;
+  A.matrix[2][1] = -2;
+  A.matrix[2][2] = -3;
+  // 1  -1   1
+  // -38  41 -34
+  // 27 -29  24
+  matrix_t etalon_res = {0};
+  s21_create_matrix(3, 3, &etalon_res);
+  etalon_res.matrix[0][0] = 1;
+  etalon_res.matrix[0][1] = -1;
+  etalon_res.matrix[0][2] = 1;
+  etalon_res.matrix[1][0] = -38;
+  etalon_res.matrix[1][1] = 41;
+  etalon_res.matrix[1][2] = -34;
+  etalon_res.matrix[2][0] = 27;
+  etalon_res.matrix[2][1] = -29;
+  etalon_res.matrix[2][2] = 24;
+  matrix_t res = {0};
+  int status_code = s21_inverse_matrix(&A, &res);
+  for (int i = 0; i < etalon_res.rows; i++)
+    for (int j = 0; j < etalon_res.columns; j++)
+      ck_assert_double_eq(res.matrix[i][j], etalon_res.matrix[i][j]);
+  ck_assert_int_eq(status_code, OK);
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&etalon_res);
+  s21_remove_matrix(&res);
+}
+END_TEST
+
 #endif
