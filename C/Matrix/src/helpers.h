1b3a19d3743dc68405532600eaaf87f9771f1d45
Кирилл Зикевский <2309696@mail.ru>
2024-04-29 17:02:10 +0300

    dev: project Matrix in progress

--- C/Matrix/src/helpers.h
+++ C/Matrix/src/helpers.h
@@ -3,6 +3,11 @@
 
 #include "s21_matrix.h"
 
-
+int is_inf_or_nan(const matrix_t *A);
+int is_equal(const matrix_t *A, const matrix_t *B, int row, int col);
+int is_incorrect_mat(const matrix_t *A);
+int is_diff_size(const matrix_t *A, const matrix_t *B);
+void mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+void get_minor(matrix_t *A, matrix_t *temp, int ex_row, int ex_col);
 
 #endif
