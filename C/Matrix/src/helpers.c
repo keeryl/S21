d33c4b77cd2f88ae7381fea3ffb8219fee3f959d
Кирилл Зикевский <2309696@mail.ru>
2024-04-29 17:03:00 +0300

    dev: project Matrix in progress

--- C/Matrix/src/helpers.c
+++ C/Matrix/src/helpers.c
@@ -12,3 +12,9 @@ int is_inf_or_nan(const matrix_t *A) {
     }
   return res;
 }
+
+int is_equal(const matrix_t *A, const matrix_t *B, int row, int col) {
+  int res = 1;
+  if (fabs(A->matrix[row][col] - B->matrix[row][col]) > 1e-08) res = 0;
+  return res;
+}
