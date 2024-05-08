58b9ed6fda8a0a4cf991d0984a78fac46209aeeb
Кирилл Зикевский <2309696@mail.ru>
2024-05-08 19:16:20 +0300

    dev: Matrix++ project in progress

--- C/Matrix++/src/s21_matrix_oop.cc
+++ C/Matrix++/src/s21_matrix_oop.cc
@@ -5,3 +5,9 @@ void S21Matrix::allocate_memory_() {
   for (int i = 0; i < this->rows_; i++)
     this->matrix_[i] = new double[this->cols_];
 }
+
+const double& S21Matrix::get_matrix_element(int row, int col) const {
+  if (row >= rows_ || col >= cols_ || row < 0 || col < 0 || !matrix_)
+    throw std::out_of_range("Incorrect input for (), index is out of range.");
+  return this->matrix_[row][col];
+}
