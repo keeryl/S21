cec24af97ac0f54a86d37dbfd741e5b95f831a41
Кирилл Зикевский <2309696@mail.ru>
2024-05-21 17:01:12 +0300

    dev: Matrix++ project in progress

--- C++/Matrix++/src/s21_matrix_oop.cc
+++ C++/Matrix++/src/s21_matrix_oop.cc
@@ -175,3 +175,22 @@ void S21Matrix::set_cols(int cols) {
 }
 
 double** S21Matrix::get_matrix() { return this->matrix_; }
+
+bool S21Matrix::EqMatrix(const S21Matrix& other) {
+  bool res = true;
+  if (this->rows_ != other.rows_ || this->cols_ != other.cols_) {
+    res = false;
+  }
+  for (int i = 0; i < this->rows_ && res; i++) {
+    for (int j = 0; j < this->cols_ && res; j++) {
+      if (this->isinf_or_nan() || other.isinf_or_nan()) {
+        res = false;
+        continue;
+      }
+      if (fabs((*this)(i, j) - other(i, j)) > 1e-08) {
+        res = false;
+      }
+    }
+  }
+  return res;
+}
