cbd1c45480f1444b48e1a24f4976f0ba1b27f753
Кирилл Зикевский <2309696@mail.ru>
2024-06-06 16:48:33 +0300

    Matrix++ project in progress

--- C++/Matrix++/src/s21_matrix_oop.test.h
+++ C++/Matrix++/src/s21_matrix_oop.test.h
@@ -351,4 +351,14 @@ TEST(SubMatrix, nan_test_1) {
   EXPECT_THROW(matrix_1.SubMatrix(matrix_2), std::logic_error);
 }
 
+TEST(SubMatrix, nan_test_2) {
+  S21Matrix matrix_1(2, 2);
+  S21Matrix matrix_2(2, 1);
+  for (int i = 0; i < matrix_1.get_rows(); i++)
+    for (int j = 0; j < matrix_1.get_cols(); j++) matrix_1(i, j) = NAN;
+  for (int i = 0; i < matrix_2.get_rows(); i++)
+    for (int j = 0; j < matrix_2.get_cols(); j++) matrix_2(i, j) = 1;
+  EXPECT_THROW(matrix_1.SubMatrix(matrix_2), std::logic_error);
+}
+
 #endif
